88ff16c70d7b022cb93f2d3d1a4a5fe0281e97a8
PirasanKugaseelan <100533656+PirasanKugaseelan@users.noreply.github.com>
2023-05-09 08:47:21 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 from dotenv import find_dotenv, load_dotenv
 from flask import Flask, request
 from flask_socketio import SocketIO, send
+from flask_socketio import SocketIO, emit
 from main_proto import Chat_With_PDFs_and_Summarize
 import hashlib
 import glob
@@ -14,6 +15,7 @@ from langchain.chains.summarize import load_summarize_chain
 from modify import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
+
 # local server object sender im using.
 """ It is important to encode using b before sending it and .decode will save it properly  """
 import socket
@@ -21,28 +23,43 @@ from rich import print
 from rich.console import Console
 from rich.table import Table
 
-# Flask is a web application framework written in Python
-s = socket.socket()
-flask_app = Flask(__name__)
-socketio=SocketIO(flask_app,cors_allowed_origins="*")
 load_dotenv()  # load variables from .env file
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-s.connect(('localhost', 8000))
 
-# Create a Console instance for custom Styling
-console = Console()
-s.listen(1)
+# Initialize the chatbot
+chatbot = Chat_With_PDFs_and_Summarize()
+
+flask_app = Flask(__name__)
+socketio = SocketIO(flask_app, cors_allowed_origins="*")
+
 @socketio.on('message')
-def slack_events():
+def handle_message(data):
     """
-        call
+    Handle incoming SocketIO messages.
     """
-    return (request)
+    query = data['query']
+    answer = chatbot.ask_question(query)
+    emit('message', {'answer': answer})
+
 @flask_app.route("/pdf", methods=["POST"])
-def a():
-    current_querry=work_load()
-    return ("a")
+def handle_pdf():
+    """
+    Handle incoming PDF data.
+    """
+    pdf_data = request.data
+    page_range = request.json.get('page_range')
+    chatbot.load_document(pdf_data, page_range)
+    return 'PDF loaded.', 200
+
+@flask_app.route("/summary", methods=["GET"])
+def handle_summary():
+    """
+    Generate a summary of the loaded document.
+    """
+    summary = chatbot.summarize()
+    return {'summary': summary}, 200
+
 # Run the Flask app
 if __name__ == "__main__":
-    flask_app.run()
+    socketio.run(flask_app)
 
