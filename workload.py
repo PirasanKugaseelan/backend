d906286569000e6e1b9807b037589037cf8f5df2
PirasanKugaseelan <100533656+PirasanKugaseelan@users.noreply.github.com>
2023-05-09 20:34:09 -0400

    Update workload.py

--- workload.py
+++ workload.py
@@ -1,28 +1,28 @@
-from waitress import serve
 import os
-
+import glob
+import hashlib
+import re
+import socket
+import eventlet
 from dotenv import find_dotenv, load_dotenv
 from flask import Flask, request
-from flask_socketio import SocketIO, send
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, send, emit
+from bs4 import BeautifulSoup
+from rich import print
+from rich.console import Console
+from rich.table import Table
+from waitress import serve
 from main_proto import Chat_With_PDFs_and_Summarize
-import hashlib
-import glob
-from langchain.document_loaders import PyPDFLoader
+from langchain.document_loaders import PyPDFLoader, PDFMinerPDFasHTMLLoader
 from langchain.chat_models import ChatOpenAI
-# from langchain import OpenAI
 from langchain.docstore.document import Document
 from langchain.chains.summarize import load_summarize_chain
 from modify import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
-from flask import Flask, request, abort
-# local server object sender im using.
-""" It is important to encode using b before sending it and .decode will save it properly  """
-import socket
-from rich import print
-from rich.console import Console
-from rich.table import Table
+
+#new dependancies = evenlet
+
 flask_app = Flask(__name__)
 load_dotenv()  # load variables from .env file
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
@@ -30,6 +30,7 @@ OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 # Initialize the chatbot
 chatbot = Chat_With_PDFs_and_Summarize()
 socketio = SocketIO(flask_app, cors_allowed_origins="*")
+
 @flask_app.route("/pdf", methods=["POST"])
 def handle_pdf():
     """
@@ -43,7 +44,10 @@ def handle_pdf():
     pdf_path = os.path.join('Docs', '{}.pdf'.format(hashlib.sha256(pdf_file.read()).hexdigest()))  # create path for the PDF file
     pdf_file.seek(0)  # reset file pointer to the beginning
     pdf_file.save(pdf_path)  # save the PDF file
-    chatbot.load_document(pdf_path, page_range)  # load the document
+
+    # Load the selected document with the specified page range
+    chatbot.load_document(pdf_path, page_range=page_range)
+
     return 'PDF loaded.', 200
 
 @socketio.on('message')
@@ -52,7 +56,9 @@ def handle_message(data):
     Handle incoming SocketIO messages.
     """
     query = data['query']
-    answer = chatbot.ask_question(query)  # ask the chatbot a question
+
+    # Ask the chatbot a question
+    answer = chatbot.ask_question(query)
     emit('message', {'answer': answer})
 
 @flask_app.route("/summary", methods=["GET"])
@@ -63,13 +69,7 @@ def handle_summary():
     summary = chatbot.summarize()  # generate a summary
     return {'summary': summary}, 200
 
-# Run the Flask app
 if __name__ == "__main__":
-    from waitress import serve
-    from eventlet import monkey_patch
+    from eventlet import monkey_patch, wsgi
     monkey_patch()
-
-    import eventlet
-    import eventlet.wsgi
-
-    eventlet.wsgi.server(eventlet.listen(('0.0.0.0', 5000)), flask_app)
+    wsgi.server(eventlet.listen(('0.0.0.0', 5000)), flask_app)
